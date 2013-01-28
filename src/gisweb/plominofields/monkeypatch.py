d0bbb4d202d8c4b4b8b9f12e5ff939a15296191e
Silvio Tomatis <silviot@gmail.com>
2013-01-28 14:31:56 +0100

    Update monkey patch to TemporaryDocument.__init__ to make it compatible with new Plomino versions

--- src/gisweb/plominofields/monkeypatch.py
+++ src/gisweb/plominofields/monkeypatch.py
@@ -163,16 +163,20 @@ PlominoView.search_json = search_json
 PlominoView.supported_query_operators = op_match
 
 from persistent.dict import PersistentDict
-def TemporaryDocument__init__(self, parent, form, REQUEST, real_doc=None):
+def TemporaryDocument__init__(self, parent, form, REQUEST, real_doc=None, validation_mode=False):
     self._parent=parent
     if real_doc is not None:
-        self.items=PersistentDict(real_doc.items)
+        self.items=PersistentDict(real_doc.items)  # This is the patched line (see commit 1ee022da)
         self.real_id=real_doc.id
     else:
         self.items={}
         self.real_id="TEMPDOC"
     self.setItem('Form', form.getFormName())
-    form.readInputs(self, REQUEST)
+    # Try to be compatible across Plomino versions
+    try:
+        form.readInputs(self, REQUEST)
+    except TypeError:
+        form.readInputs(self, REQUEST, validation_mode=validation_mode)
     self._REQUEST=REQUEST
 from Products.CMFPlomino.PlominoDocument import TemporaryDocument
 TemporaryDocument.__init__ = TemporaryDocument__init__
