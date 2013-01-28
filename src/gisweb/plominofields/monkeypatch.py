96c8ed528089a43e63a83cd72400354c0a3974c0
Silvio Tomatis <silviot@gmail.com>
2013-01-28 16:39:19 +0100

    Upgrade to patch Plomino 1.16.4

--- src/gisweb/plominofields/monkeypatch.py
+++ src/gisweb/plominofields/monkeypatch.py
@@ -19,35 +19,41 @@ PlominoIndex.security.declareProtected(READ_PERMISSION, 'getFullLayout')
 InitializeClass(PlominoView)
 InitializeClass(PlominoForm)
 
-# NEW VERSION 1.13.3
-def readInputs(self, doc, REQUEST, process_attachments=False, applyhidewhen=True):
-    """ read submitted values in REQUEST and store them in document according
-    fields definition
-    """
-    all_fields = self.getFormFields(includesubforms=True, doc=doc, applyhidewhen=False)
-#    if applyhidewhen:
-#        displayed_fields = self.getFormFields(includesubforms=True, doc=doc, applyhidewhen=True)
+# VERSION 1.16.4
+def readInputs(self, doc, REQUEST, process_attachments=False, applyhidewhen=True, validation_mode=False):
+        """ Read submitted values in REQUEST and store them in document
+        according to fields definition.
+        """
+        all_fields = self.getFormFields(includesubforms=True, doc=doc, request=REQUEST)
+        # Commenting the next two lines is the purpose of this MonkeyPatch
+        # if applyhidewhen:
+        #     displayed_fields = self.getFormFields(includesubforms=True, doc=doc, applyhidewhen=True, request=REQUEST)
 
-    for f in all_fields:
-        mode = f.getFieldMode()
-        fieldName = f.id
-        if mode=="EDITABLE":
-            submittedValue = REQUEST.get(fieldName)
-            if submittedValue is not None:
-                if submittedValue=='':
-                    doc.removeItem(fieldName)
+        for f in all_fields:
+            mode = f.getFieldMode()
+            fieldName = f.id
+            if mode=="EDITABLE":
+                submittedValue = REQUEST.get(fieldName)
+                if submittedValue is not None:
+                    if submittedValue=='':
+                        doc.removeItem(fieldName)
+                    else:
+                        v = f.processInput(submittedValue, doc, process_attachments, validation_mode=validation_mode)
+                        doc.setItem(fieldName, v)
                 else:
-                    v = f.processInput(submittedValue, doc, process_attachments)
-                    doc.setItem(fieldName, v)
-#            else:
-#                #the field was not submitted, probably because it is not part of the form (hide-when, ...)
-#                #so we just let it unchanged, but with SELECTION or DOCLINK, we need to presume it was empty
-#                #(as SELECT/checkbox/radio tags do not submit an empty value, they are just missing
-#                #in the querystring)
-#                if applyhidewhen and f in displayed_fields:
-#                    fieldtype = f.getFieldType()
-#                    if fieldtype == "SELECTION" or fieldtype == "DOCLINK":
-#                        doc.removeItem(fieldName)
+                    pass
+                    # The field was not submitted, probably because it is
+                    # not part of the form (hide-when, ...) so we just leave
+                    # it unchanged. But with SELECTION or DOCLINK, we need
+                    # to presume it was empty (as SELECT/checkbox/radio tags
+                    # do not submit an empty value, they are just missing
+                    # in the querystring)
+
+                    # Commenting the next four lines is the purpose of this MonkeyPatch
+                    # if applyhidewhen and f in displayed_fields:
+                    #     fieldtype = f.getFieldType()
+                    #     if fieldtype == "SELECTION" or fieldtype == "DOCLINK":
+                    #         doc.removeItem(fieldName)
 
 PlominoForm.readInputs = readInputs
 
